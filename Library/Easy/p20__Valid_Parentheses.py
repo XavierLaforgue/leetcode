c82a1fc194c32051ffda6188ff1bd1515fe03033
Xavier Laforgue <xavierjimmys@gmail.com>
2026-06-24 23:44:35 +0200

    Add draft of solution for leetcode problem 20: Valid Parentheses

---
+++ Library/Easy/p20__Valid_Parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        lst = []
+        opening = ('(', '[', '{')
+        closing = (')', ']', '}')
+        for parenthesis in s:
+            if parenthesis in closing and not lst:
+                return False
+            if parenthesis in opening \
+                    or lst[-1] != opening[closing.index(parenthesis)]:
+                lst.append(parenthesis)
+            elif lst[-1] == opening[closing.index(parenthesis)]:
+                lst.pop()
+        return bool(lst)
---
+++ test_p20__Valid_Parentheses.py
@@ -0,0 +1,6 @@
+import unittest
+import p20__Valid_Parentheses
+
+
+class TestValidParentheses(unittest.TestCase):
+    def test_parenthesis_pair(self):
