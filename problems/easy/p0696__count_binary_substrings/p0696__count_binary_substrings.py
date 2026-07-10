318d566eef140a82e394d713d588c4deacb53d75
Xavier Laforgue <xavierjimmys@gmail.com>
2026-07-10 22:43:49 +0200

    Add solution to leetcode problem 696: count binary substrings

---
+++ problems/easy/p0696__count_binary_substrings/p0696__count_binary_substrings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+        count_prev = 0
+        count_curr = 1
+        count_subs = 0
+        for idx, char in enumerate(s[1:], 1):
+            if char == s[idx-1]:
+                count_curr += 1
+                if count_prev >= count_curr:
+                    count_subs += 1
+            else:
+                count_subs += 1
+                count_prev = count_curr
+                count_curr = 1
+        return count_subs
+
+
+if __name__ == "__main__":
+    print(Solution().countBinarySubstrings("10101"))
---
+++ problems/easy/p0696__count_binary_substrings/p0696__count_binary_substrings_tests.py
@@ -0,0 +1,15 @@
+import unittest
+from p0696__count_binary_substrings import Solution
+
+
+class TestCountBinarySubstring(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sol = Solution()
+        return super().setUp()
+
+    def test1(self):
+        self.assertEqual(self.sol.countBinarySubstrings("00110011"), 6)
+
+    def test2(self):
+        self.assertEqual(self.sol.countBinarySubstrings("10101"), 4)
+
