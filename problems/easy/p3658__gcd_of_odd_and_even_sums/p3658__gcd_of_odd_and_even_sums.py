4330055ceacb26c4f810d67f3820946e01e5c41d
Xavier Laforgue <xavierjimmys@gmail.com>
2026-07-16 00:57:25 +0200

    Add solution to leetcode problem 3658: gcd of odd and even sums. Using Euclid's algorithm

--- problems/easy/p3658__gcd_of_odd_and_even_sums/p3658__gcd_of_odd_and_even_sums.py
+++ problems/easy/p3658__gcd_of_odd_and_even_sums/p3658__gcd_of_odd_and_even_sums.py
@@ -1,3 +1,16 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
-        return 0
+        sumOdd: int = sum(2 * m + 1 for m in range(n))
+        sumEven: int = sum(2 * (m + 1) for m in range(n))
+        # Euclid algorithm:
+        greater = sumOdd
+        lesser = sumEven - sumOdd
+        while greater != lesser:
+            if greater > lesser:
+                greater, lesser = lesser, greater - lesser
+            else:
+                greater, lesser = greater, lesser - greater
+        return greater
+
+if __name__ == "__main__":
+    Solution().gcdOfOddEvenSums(3)
