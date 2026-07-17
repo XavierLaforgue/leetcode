8520b24b4bf0a1db6602be18e8f4ada34664ef36
Xavier Laforgue <xavierjimmys@gmail.com>
2026-07-18 00:57:50 +0200

    Add solution to leetcode problem 693: binary number with alternating bits

--- problems/easy/p0693__binary_number_with_alternating_bits/p0693__binary_number_with_alternating_bits.py
+++ problems/easy/p0693__binary_number_with_alternating_bits/p0693__binary_number_with_alternating_bits.py
@@ -1,3 +1,8 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        return True
+        alternating = n & 1 != (n >> 1) & 1
+        while alternating:
+            n >>= 1
+            alternating = n & 1 != (n >> 1) & 1
+        return n == 0
+    
